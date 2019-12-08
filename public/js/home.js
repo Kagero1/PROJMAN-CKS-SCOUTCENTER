f9e92a7dc8aa2fe6f3375173c7963a78a91b2666
Kagero1 <charles_stepven_lin@dlsu.edu.ph>
2019-12-08 22:55:02 +0800

    improved views

--- public/js/home.js
+++ public/js/home.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     $('#inventory').bdt();
     $('#requestUser').bdt();
+    $('#requestStatus').bdt();
     $('.dataTables_length').addClass('bs-select');
-
   });
