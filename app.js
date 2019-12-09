b27964b3fbc02220db51dae1b2b399ef63018baf
MarkLimML <mark_lim@dlsu.edu.ph>
2019-12-09 22:50:20 +0800

    .

--- app.js
+++ app.js
@@ -5,6 +5,19 @@ const session = require("express-session")
 const cparser = require("cookie-parser")
 const app = express()
 
+var mysql = require("mysql");
+var connection = mysql.createConnection({
+  host: 'localhost',
+  port: '3306',
+  user: 'root',
+  password: 'p@ssword',
+  database: 'projman'
+});
+connection.connect((err) => {
+  if (err) { console.log(err); }
+  else { console.log('Connected!') }
+});
+
 hbs.registerPartials(__dirname + "/views/partials", ()=>{
     console.log("Partials are loaded successfully")
 })
@@ -36,9 +49,7 @@ app.use(bparser.urlencoded({limit: "50mb", extended: true}))
 app.get("/", (req, res)=>{
     res.render("login.hbs");
 })
-app.get("/home", (req, res)=>{
-    res.render("home.hbs");
-})
+
 app.get("/inventory", (req, res)=>{
     res.render("inventory.hbs");
 })
@@ -48,6 +59,29 @@ app.get("/requestItems", (req, res)=>{
 app.get("/requestStatus", (req, res)=>{
     res.render("request-status.hbs");
 })
+
+app.post("/home", (req, res)=>{
+    let troopNo = req.body.loginTroopNumber
+    let password = req.body.loginPassword
+    
+        connection.query("SELECT * FROM projman.users WHERE troopno = "+troopNo+";", function (err, result, fields) {
+          if (err) {}
+          else if(result) {
+              if(result.password != password) { alert("Wrong Password!");}
+              else
+                console.log(result);
+                res.render("home.hbs");
+          }
+          else {
+            alert("User does not exist!");
+          }
+        }).on('error', function(err) {
+          console.log("[mysql error]",err);
+        });
+      
+    
+})
+
 
 //Listen
 app.listen(process.env.PORT || 3000, function(){
